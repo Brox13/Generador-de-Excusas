e685e263f62a2ef9e72e151654034b00848fad71
Brox13 <a_gomez@hotmail.es>
2022-08-22 10:44:54 +0000

    terminado y traducido a español

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+};
+let generateExcuse = () => {
+  let who = ["Mi perro", "Mi abuela", "Su tortuga", "Mi pajaro"];
+  let action = ["comió", "orina", "aplasto", "rompió"];
+  let what = ["mi tarea", "las llaves", "el coche"];
+  let when = [
+    "antes de la clase",
+    "justo a tiempo",
+    "cuando terminé",
+    "durante mi almuerzo",
+    "mientras estaba orando"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
